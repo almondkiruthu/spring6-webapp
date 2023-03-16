2002d6a7163375a2aee6c7fd3ed9614df8339349
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-03-16 18:52:23 +0300

    Modified the newly created Service controller

--- src/main/java/guru/springframework/almond/spring6webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/almond/spring6webapp/controllers/BookController.java
@@ -3,12 +3,24 @@ package guru.springframework.almond.spring6webapp.controllers;
 import guru.springframework.almond.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.almond.spring6webapp.services.BookService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookController {
+
+    private final BookService bookService;
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
 
-    private final BookService bookService;
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookService.findAll());
+
+        return "books";
+    }
+
 }
